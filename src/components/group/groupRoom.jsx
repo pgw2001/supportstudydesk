b9780f8ba0c4419e43e059d4cff89977134db798
정재현 <h20026@naver.com>
2026-05-23 18:38:30 +0900

    [수정] Todo 리스트 및 성장화분 기능 error 해결

--- src/components/group/groupRoom.jsx
+++ src/components/group/groupRoom.jsx
@@ -253,6 +253,19 @@ function GroupRoom({
     user,
     username,
   ]);
+   useEffect(() => {
+  let interval;
+
+  if (isStudying) {
+    interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+  }
+
+  return () => {
+    clearInterval(interval);
+  };
+}, [isStudying]);
 
   useEffect(() => {
     if (!group?.id) return;
